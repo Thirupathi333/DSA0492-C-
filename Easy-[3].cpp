2c707d74261f71a2807e79024166f82f5af26521
Thirupathi333 <123434039+Thirupathi333@users.noreply.github.com>
2024-03-23 14:47:49 +0530

    Create Easy-[3].cpp

---
+++ Easy-[3].cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+    int myInt = 5;
+    float myFloat;
+    myFloat = myInt;
+    cout << "Integer variable: " << myInt << endl;
+    cout << "Float variable after assignment: " << myFloat << endl;
+
+    return 0;
+}
